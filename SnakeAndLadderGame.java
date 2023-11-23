f6c6c42ce10b732f9e1921e657af9ce708c29bdd
prarthanagalani <prarthanagalani@gmail.com>
2023-11-23 11:47:45 +0530

    added: initial position of player-day3

--- SnakeAndLadderGame.java
+++ SnakeAndLadderGame.java
@@ -1,9 +1,18 @@
 public class SnakeAndLadderGame {
 
-    
+    private int playerPosition;
+
+    // Use Case 1 : initialize position of player with 0.
+    public SnakeAndLadderGame() {
+        this.playerPosition = 0;
+    }
 
     public static void main(String[] args) {
 
-       System.out.println("Welcome to Snake and Ladder game!!");
+        System.out.println("Welcome to Snake and Ladder game!!");
+
+        // Use Case 1: Snake and Ladder game played with a single player at start
+        // position 0
+        SnakeAndLadderGame game = new SnakeAndLadderGame();
     }
 }
